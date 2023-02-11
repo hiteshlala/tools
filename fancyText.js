78703ae0512e01276082ecbff2e2ad9364010955
hiteshlala <hiteshlala@gmail.com>
2023-02-11 14:00:14 -0800

    add snippts for check marks

---
+++ fancyText.js
@@ -0,0 +1,24 @@
+const figlet = require('figlet');
+
+
+// figlet.fonts(function(err, fonts) {
+//   if (err) {
+//       console.log('something went wrong...');
+//       console.dir(err);
+//       return;
+//   }
+//   console.dir(fonts);
+
+//   for(let i = 0; i < fonts.length; i++) {
+//     const font = fonts[i];
+//     console.log(font)
+//     console.log(figlet.textSync('Hitesh Lala', { font }))
+//   }
+// });
+
+console.log(figlet.textSync('Testing Git Hook', { font: 'Pagga' }))
+
+
+
+
+
